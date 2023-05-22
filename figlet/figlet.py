8b0dbb9c34cd3fd677f9e30156cf2a0429194366
PB Github <phuong.buitan95@gmail.com>
2023-05-22 13:02:16 +0700

    complete figlet.py

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,13 +1,37 @@
+import sys
 from pyfiglet import Figlet
 from random import randrange
 
-figlet = Figlet()
-fonts = figlet.getFonts()
-# for font in fonts:
-#     print(font)
-randFontNum = randrange(len(fonts))
-print(f"Font {randFontNum} is {fonts[randFontNum]}")
+def checkCommandLine():
+    # print(sys.argv) # check elements after the "command word" in command-line
+    if len(sys.argv)==3 and ("-f" or "--font") in sys.argv[1]:
+        # print("Not Random Font")
+        figletExecution(sys.argv[2])
+    elif len(sys.argv)==1:
+        # print("Random Font")
+        figletExecution()
+    else:
+        print("Invalid usage")
+        return sys.exit(1)
 
-figlet.setFont(font=fonts[randFontNum])
-input_str = input("Input: ")
-output_str = print(f"Output: {figlet.renderText(input_str)}")
+def figletExecution(f=None):
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+    if f is None:    
+        randFontNum = randrange(len(fonts))
+        # print(f"Font {randFontNum} is {fonts[randFontNum]}")
+        figlet.setFont(font=fonts[randFontNum])
+    else:
+        if f not in fonts:
+            print("Invalid usage")
+            return sys.exit(1)
+        else:
+            figlet.setFont(font=f)
+    
+    input_str = input("Input: ")
+    # Output
+    return print(f"Output: {figlet.renderText(input_str)}")
+
+# Main
+if __name__ == "__main__":
+    checkCommandLine()
